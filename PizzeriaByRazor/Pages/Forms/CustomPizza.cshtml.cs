25e7e97c44f5527fdc77fa1c7ff1a5e7e0586788
Kristian Skrypkaru <kristian.skrypkaru@outlook.com>
2023-01-02 12:30:40 +0200

    added IActionResult method for create pizza

--- PizzeriaByRazor/Pages/Forms/CustomPizza.cshtml.cs
+++ PizzeriaByRazor/Pages/Forms/CustomPizza.cshtml.cs
@@ -8,8 +8,23 @@ namespace PizzeriaByRazor.Pages.Forms
     {
         [BindProperty]
         public PizzasModel Pizza { get; set; }
+        public float PizzaPirce { get; set; }
+
         public void OnGet()
         {
+        }
+        public IActionResult OnPost()
+        {
+            PizzaPirce = Pizza.BasePrice;
+            if (Pizza.TomatoSauce) PizzaPirce += 1;
+            if (Pizza.Cheese) PizzaPirce += 1;
+            if (Pizza.Peperoni) PizzaPirce += 1;
+            if (Pizza.Mushroom) PizzaPirce += 1;
+            if (Pizza.Tuna) PizzaPirce += 1;
+            if (Pizza.Pineapple) PizzaPirce += 7;
+            if (Pizza.Ham) PizzaPirce += 1;
+            if (Pizza.Beef) PizzaPirce += 1;
+            return RedirectToPage("/Checkout/Checkout", new { Pizza.PizzaName, PizzaPirce});
         }
     }
 }
