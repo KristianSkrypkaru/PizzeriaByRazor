a4dc1dd52955a6839dc87c96ae9a160f2e8321ca
Kristian Skrypkaru <kristian.skrypkaru@outlook.com>
2023-01-02 12:17:13 +0200

    added model by mvvm pattern

--- PizzeriaByRazor/Pages/Forms/CustomPizza.cshtml.cs
+++ PizzeriaByRazor/Pages/Forms/CustomPizza.cshtml.cs
@@ -1,10 +1,13 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using PizzeriaByRazor.Models;
 
 namespace PizzeriaByRazor.Pages.Forms
 {
     public class CustomPizzaModel : PageModel
     {
+        [BindProperty]
+        public PizzasModel Pizza { get; set; }
         public void OnGet()
         {
         }
