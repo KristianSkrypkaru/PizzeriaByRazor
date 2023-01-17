62b4ecfcde7e2677fa3d83f56ed2db0b18bfd269
Kristian Skrypkaru <kristian.skrypkaru@outlook.com>
2023-01-17 22:23:19 +0200

    added PizzaOrderModel

--- PizzeriaByRazor/Pages/Checkout/Checkout.cshtml.cs
+++ PizzeriaByRazor/Pages/Checkout/Checkout.cshtml.cs
@@ -1,5 +1,7 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using PizzeriaByRazor.Data;
+using PizzeriaByRazor.Models;
 
 namespace PizzeriaByRazor.Pages.Checkout
 {
@@ -9,6 +11,13 @@ namespace PizzeriaByRazor.Pages.Checkout
         public string PizzaName { get; set; }
         public float PizzaPrice { get; set; }
         public string ImageTitle{ get; set; }
+
+        private readonly ApplicationDbContext _context;
+
+        public CheckoutModel(ApplicationDbContext context)
+        {
+            _context = context;
+        }
         public void OnGet()
         {
             if (string.IsNullOrWhiteSpace(PizzaName))
@@ -19,6 +28,14 @@ namespace PizzeriaByRazor.Pages.Checkout
             {
                 ImageTitle = "Create";
             }
+
+            PizzaOrder pizzaOrder = new PizzaOrder();
+            pizzaOrder.PizzaName = PizzaName;
+            pizzaOrder.BasePrice = PizzaPrice;
+
+            _context.PizzaOrders.Add(pizzaOrder);
+            _context.SaveChanges();
         }
+        
     }
 }
---
+++ PizzeriaByRazor/Pages/Orders.cshtml.cs
@@ -0,0 +1,23 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using PizzeriaByRazor.Data;
+using PizzeriaByRazor.Models;
+
+namespace PizzeriaByRazor.Pages
+{
+    public class OrdersModel : PageModel
+    {
+        public List<PizzaOrder> PizzaOrders = new List<PizzaOrder>();
+
+        private readonly ApplicationDbContext _context;
+        
+        public OrdersModel(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+        public void OnGet()
+        {
+            PizzaOrders = _context.PizzaOrders.ToList();
+        }
+    }
+}
